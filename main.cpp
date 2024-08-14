4082c357c3dae7a3fd8e5985332eb2a34aebc9c9
OndraNeveril <ondra.neveril@centrum.cz>
2024-08-14 16:15:10 +0200

    Porovnani

--- main.cpp
+++ main.cpp
@@ -11,5 +11,12 @@ int main()
     cin >> zadano;
     cout << "Jeho dvojnasobek je " << 2 * zadano << endl;
 
+    cout << "Zadej dve cela cisla: ";
+    int m, n, vysledek;
+    cin >> m >> n;
+    cout << "Vetsi je " << ((m > n) ? m : n) << endl;
+
+    /* slo by i pøes if else*/
+
     return 0;
 }
