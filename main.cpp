47b4428657826acfa2f9d8e6347ebf7dc0f097be
OndraNeveril <ondra.neveril@centrum.cz>
2024-08-14 16:09:13 +0200

    Dvojnasobek

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+
+    cout << "Zadej cele cislo: ";
+    int zadano;
+    cin >> zadano;
+    cout << "Jeho dvojnasobek je " << 2 * zadano << endl;
+
     return 0;
 }
